6010f405c8ecd9f271120989253c95b18dcf31ea
millenniumlk <aflalahd2000@gmail.com>
2026-08-06 21:17:42 +0530

    chore: update Auth.tsx google oauth redirect

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -269,7 +269,7 @@ const Auth = () => {
       const { error } = await supabase.auth.signInWithOAuth({ 
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}/auth/callback`,
+          redirectTo: window.location.origin,
           queryParams: {
             prompt: 'consent'
           }
