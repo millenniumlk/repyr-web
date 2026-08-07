b5e0c548c8fca49b6a3968c39722bbb4efcdc304
millenniumlk <aflalahd2000@gmail.com>
2026-08-07 19:18:39 +0530

    Remove AnimatePresence from MainLayout to force instant unmount and prevent deadlocks

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -212,18 +212,15 @@ const MainLayout = () => {
       {/* Main Content */}
       <main className="flex-1 flex flex-col min-w-0 bg-transparent">
         <div className="md:p-8 flex-1 max-w-7xl mx-auto w-full relative">
-          <AnimatePresence>
             <motion.div
               key={location.pathname}
               initial={{ opacity: 0, y: 10 }}
               animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -10, position: 'absolute' }}
               transition={{ duration: 0.2 }}
-              className="w-full h-full top-0 left-0 md:top-8 md:left-8 md:w-[calc(100%-4rem)]"
+              className="w-full h-full"
             >
               {currentOutlet}
             </motion.div>
-          </AnimatePresence>
         </div>
       </main>
 
